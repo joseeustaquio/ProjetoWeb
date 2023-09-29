32bd160a26c826a316691c76c398f2a4d81c8e12
Jose Eustaquio <taquinhomz@gmail.com>
2023-09-29 16:56:21 -0300

    Inserindo menu.js e alterando as classes cliente e style

---
+++ exercicio2/js/menu.js
@@ -0,0 +1,6 @@
+function abrirMenu(){
+    document.getElementById("menu").classList.add("menuAberto");
+}
+function fecharMenu(){
+    document.getElementById("menu").classList.remove("menuAberto");
+}
