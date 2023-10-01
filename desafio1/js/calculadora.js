ce5788bb528a96883f0d07a02cab9725dac920f0
Jose Eustaquio <taquinhomz@gmail.com>
2023-10-01 17:46:36 -0300

    atualizando calculadora html e js

--- desafio1/js/calculadora.js
+++ desafio1/js/calculadora.js
@@ -1,37 +1,44 @@
-
-function somar() {
+class Calculadora {
+ somar() {
     var n1 = parseFloat(document.getElementById("n1").value);
     var n2 = parseFloat(document.getElementById("n2").value);
     var resultado = n1 + n2;
     alert(resultado);
 
 }
-function subtrair() {
+subtrair() {
     var n1 = parseFloat(document.getElementById("n1").value);
     var n2 = parseFloat(document.getElementById("n2").value);
-    var resultado = n1 - n2; 
-   alert(resultado);
+    var resultado = n1 - n2;
+    alert(resultado);
 
 }
-function multiplicar() {
+ multiplicar() {
     var n1 = parseFloat(document.getElementById("n1").value);
     var n2 = parseFloat(document.getElementById("n2").value);
     var resultado = n1 * n2;
     alert(resultado);
 
 }
-function dividir() {
+ dividir() {
     var n1 = parseFloat(document.getElementById("n1").value);
     var n2 = parseFloat(document.getElementById("n2").value);
     var resultado = n1 / n2;
     alert(resultado);
 
 }
-function porcentagem() {
+  porcentagem() {
     var n1 = parseFloat(document.getElementById("n1").value);
     var n2 = parseFloat(document.getElementById("n2").value);
-    var resultado = n1 % n2;
+    var resultado = n1 * n2 / 100;
     alert(resultado);
+  }
 
 }
+var calc = new  Calculadora();
+/*alert(calc.somar());
+alert(calc.subtrair());
+alert(calc.multiplicar());
+alert(calc.dividir());
+alert(calc.porcentagem());*/
 
