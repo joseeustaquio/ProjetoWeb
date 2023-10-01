14e4491d1c87fbf0a05e970764fd7c1aeb898e1b
Jose Eustaquio <taquinhomz@gmail.com>
2023-10-01 17:04:34 -0300

    inserindo javascript em calculqdora

---
+++ desafio1/js/calculadora.js
@@ -0,0 +1,37 @@
+
+function somar() {
+    var n1 = parseFloat(document.getElementById("n1").value);
+    var n2 = parseFloat(document.getElementById("n2").value);
+    var resultado = n1 + n2;
+    alert(resultado);
+
+}
+function subtrair() {
+    var n1 = parseFloat(document.getElementById("n1").value);
+    var n2 = parseFloat(document.getElementById("n2").value);
+    var resultado = n1 - n2; 
+   alert(resultado);
+
+}
+function multiplicar() {
+    var n1 = parseFloat(document.getElementById("n1").value);
+    var n2 = parseFloat(document.getElementById("n2").value);
+    var resultado = n1 * n2;
+    alert(resultado);
+
+}
+function dividir() {
+    var n1 = parseFloat(document.getElementById("n1").value);
+    var n2 = parseFloat(document.getElementById("n2").value);
+    var resultado = n1 / n2;
+    alert(resultado);
+
+}
+function porcentagem() {
+    var n1 = parseFloat(document.getElementById("n1").value);
+    var n2 = parseFloat(document.getElementById("n2").value);
+    var resultado = n1 % n2;
+    alert(resultado);
+
+}
+
